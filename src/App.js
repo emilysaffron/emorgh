2dbc762c151fc8b1af2f3753f7dea7f9aed4709e
emilysaffron <emiy.saffron@bbc.co.uk>
2020-11-30 15:03:43 +0000

    basic next button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Data from "./data";
+import Data from "./Data";
+import React, { useState } from "react";
+
 function App() {
+  const [pageNo, updatePage] = useState(1);
+
+  const loadArticle = () => {
+    updatePage(pageNo + 1);
+    console.log(pageNo);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <Data />
-      </header>
+      <Data num={pageNo} />
+
+      <button onClick={loadArticle}></button>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { Headline } from "@bbc/psammead-headings";
 import Paragraph from "@bbc/psammead-paragraph";
-function Data() {
+function Data({ num }) {
   const [article, setArticle] = useState("");
+  let data = "";
   const getData = () => {
-    const data = require("./data/article-1.json");
+    switch (num) {
+      case 1:
+        data = require("./data/article-1.json");
+        break;
+      case 2:
+        data = require("./data/article-2.json");
+        break;
+      case 3:
+        data = require("./data/article-3.json");
+        break;
+      case 4:
+        data = require("./data/article-4.json");
+        break;
+      case 5:
+        data = require("./data/article-5.json");
+        break;
+      default:
+        break;
+    }
+
     setArticle(data.body);
   };
   useEffect(() => {
     getData();
-  }, []);
+  }, [num]);
 
   return article ? (
     <div>
