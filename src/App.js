93e0f2231e4f299b4331e326464494b39deced45
emilysaffron <emiy.saffron@bbc.co.uk>
2020-11-30 16:16:36 +0000

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import Data from "./Data";
 import React, { useState } from "react";
-
+import { CanonicalMenuButton } from "@bbc/psammead-navigation/dropdown";
+import { ScrollableNavigation } from "@bbc/psammead-navigation/scrollable";
+import Navigation, {
+  NavigationUl,
+  NavigationLi,
+} from "@bbc/psammead-navigation";
 function App() {
   const [pageNo, updatePage] = useState(1);
 
-  const loadArticle = () => {
-    updatePage(pageNo + 1);
-    console.log(pageNo);
+  const loadNextArticle = () => {
+    if (pageNo < 5) {
+      updatePage(pageNo + 1);
+    }
+  };
+
+  const loadPreviousArticle = () => {
+    if (pageNo > 1) {
+      updatePage(pageNo - 1);
+    }
   };
   return (
     <div className="App">
+      <Navigation>
+        <ScrollableNavigation>
+          <NavigationUl>
+            <NavigationLi onClick={loadNextArticle}>Next</NavigationLi>
+            <NavigationLi onClick={loadPreviousArticle}>Previous</NavigationLi>
+          </NavigationUl>
+        </ScrollableNavigation>
+      </Navigation>
       <Data num={pageNo} />
-
-      <button onClick={loadArticle}></button>
     </div>
   );
 }
