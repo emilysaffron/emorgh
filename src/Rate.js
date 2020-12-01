d2195f25564270d16c57612a7a6913891e6db4e2
emilysaffron <emiy.saffron@bbc.co.uk>
2020-12-01 17:25:25 +0000

    numbered

--- src/Rate.js
+++ src/Rate.js
@@ -1,8 +1,20 @@
 import React from "react";
 import getData from "./FetchArticles";
+import { latin } from "@bbc/gel-foundations/scripts";
 import { StoryPromoLi, StoryPromoUl } from "@bbc/psammead-story-promo-list";
 import { Headline } from "@bbc/psammead-headings";
+import {
+  MostReadList,
+  MostReadLink,
+  MostReadRank,
+  MostReadItemWrapper,
+} from "@bbc/psammead-most-read";
 
+import styled from "@emotion/styled";
+
+const StyledMostRead = styled(MostReadList)`
+  display: block;
+`;
 const Rate = ({ rate }) => {
   const title1 = getData(1)[0].model.text;
   const title2 = getData(2)[0].model.text;
@@ -11,23 +23,98 @@ const Rate = ({ rate }) => {
   const title5 = getData(5)[0].model.text;
 
   return (
-    <StoryPromoUl>
-      <StoryPromoLi>
-        <Headline>{title1}</Headline>
-      </StoryPromoLi>
-      <StoryPromoLi>
-        <Headline>{title2}</Headline>
-      </StoryPromoLi>
-      <StoryPromoLi>
-        <Headline>{title3}</Headline>
-      </StoryPromoLi>
-      <StoryPromoLi>
-        <Headline>{title4}</Headline>
-      </StoryPromoLi>
-      <StoryPromoLi>
-        <Headline>{title5}</Headline>
-      </StoryPromoLi>
-    </StoryPromoUl>
+    <div>
+      <MostReadItemWrapper dir="ltr" children columnLayout="twoColumn">
+        <MostReadRank
+          service="news"
+          script={latin}
+          listIndex={1}
+          numberOfItems={1}
+          dir="ltr"
+          columnLayout="twoColumn"
+          size="small"
+        />
+        <MostReadLink
+          dir="ltr"
+          href="/bbc.co.uk/news/articles/27051997"
+          service="news"
+          script={latin}
+          title={title1}
+        />
+      </MostReadItemWrapper>
+      <MostReadItemWrapper dir="ltr" children columnLayout="twoColumn">
+        <MostReadRank
+          service="news"
+          script={latin}
+          listIndex={2}
+          numberOfItems={1}
+          dir="ltr"
+          columnLayout="twoColumn"
+          size="small"
+        />
+        <MostReadLink
+          dir="ltr"
+          href="/bbc.co.uk/news/articles/27051997"
+          service="news"
+          script={latin}
+          title={title2}
+        />
+      </MostReadItemWrapper>
+      <MostReadItemWrapper dir="ltr" children columnLayout="twoColumn">
+        <MostReadRank
+          service="news"
+          script={latin}
+          listIndex={3}
+          numberOfItems={1}
+          dir="ltr"
+          columnLayout="twoColumn"
+          size="small"
+        />
+        <MostReadLink
+          dir="ltr"
+          href="/bbc.co.uk/news/articles/27051997"
+          service="news"
+          script={latin}
+          title={title3}
+        />
+      </MostReadItemWrapper>
+      <MostReadItemWrapper dir="ltr" children columnLayout="twoColumn">
+        <MostReadRank
+          service="news"
+          script={latin}
+          listIndex={4}
+          numberOfItems={1}
+          dir="ltr"
+          columnLayout="twoColumn"
+          size="small"
+        />
+        <MostReadLink
+          dir="ltr"
+          href="/bbc.co.uk/news/articles/27051997"
+          service="news"
+          script={latin}
+          title={title4}
+        />
+      </MostReadItemWrapper>
+      <MostReadItemWrapper dir="ltr" children columnLayout="twoColumn">
+        <MostReadRank
+          service="news"
+          script={latin}
+          listIndex={5}
+          numberOfItems={1}
+          dir="ltr"
+          columnLayout="twoColumn"
+          size="small"
+        />
+        <MostReadLink
+          dir="ltr"
+          href="/bbc.co.uk/news/articles/27051997"
+          service="news"
+          script={latin}
+          title={title5}
+        />
+      </MostReadItemWrapper>
+    </div>
   );
 };
 export default Rate;
