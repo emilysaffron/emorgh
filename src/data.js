b15340f23ec86ccf6be70424f96ab1c549a23b77
emilysaffron <emiy.saffron@bbc.co.uk>
2020-12-04 12:19:13 +0000

    article 8 span grid added

--- src/data.js
+++ src/data.js
@@ -4,6 +4,7 @@ import Paragraph from "@bbc/psammead-paragraph";
 import Image from "@bbc/psammead-image";
 import BulletedList, { BulletedListItem } from "@bbc/psammead-bulleted-list";
 import Figure from "@bbc/psammead-figure";
+import Grid from "@bbc/psammead-grid";
 import styled from "@emotion/styled";
 import getData from "./FetchArticles";
 
@@ -19,20 +20,101 @@ function Data({ num }) {
   }, [num]);
 
   return article ? (
-    <div>
+    <Grid
+      enableGelGutters
+      columns={{
+        group0: 8,
+        group1: 8,
+        group2: 8,
+        group3: 8,
+        group4: 8,
+        group5: 8,
+      }}
+      margins={{
+        group0: true,
+        group1: true,
+        group2: true,
+        group3: true,
+        group4: true,
+        group5: true,
+      }}
+    >
       {article.map((item) => {
         switch (item.type) {
           case "heading":
-            return <Headline>{item.model.text}</Headline>;
+            return (
+              <Grid
+                item
+                startOffset={{
+                  group0: 2,
+                  group1: 2,
+                  group2: 2,
+                  group3: 2,
+                  group4: 2,
+                  group5: 2,
+                }}
+                columns={{
+                  group0: 8,
+                  group1: 8,
+                  group2: 8,
+                  group3: 8,
+                  group4: 8,
+                  group5: 8,
+                }}
+              >
+                <Headline>{item.model.text}</Headline>
+              </Grid>
+            );
 
           case "paragraph":
-            return <Paragraph>{item.model.text}</Paragraph>;
-
+            return (
+              <Grid
+                item
+                startOffset={{
+                  group0: 2,
+                  group1: 2,
+                  group2: 2,
+                  group3: 2,
+                  group4: 2,
+                  group5: 2,
+                }}
+                columns={{
+                  group0: 5,
+                  group1: 5,
+                  group2: 5,
+                  group3: 5,
+                  group4: 5,
+                  group5: 5,
+                }}
+              >
+                <Paragraph>{item.model.text}</Paragraph>;
+              </Grid>
+            );
           case "image":
             return (
-              <StyledFigure>
-                <Image src={item.model.url} alt={item.model.altText} />
-              </StyledFigure>
+              <Grid
+                item
+                startOffset={{
+                  group0: 2,
+                  group1: 2,
+                  group2: 2,
+                  group3: 2,
+                  group4: 2,
+                  group5: 2,
+                }}
+                columns={{
+                  group0: 8,
+                  group1: 8,
+                  group2: 8,
+                  group3: 8,
+                  group4: 8,
+                  group5: 8,
+                }}
+              >
+                <StyledFigure>
+                  <Image src={item.model.url} alt={item.model.altText} />
+                </StyledFigure>
+              </Grid>
             );
 
           case "list":
@@ -44,13 +126,35 @@ function Data({ num }) {
               );
             }
 
-            return <BulletedList>{list}</BulletedList>;
+            return (
+              <Grid
+                item
+                startOffset={{
+                  group0: 2,
+                  group1: 2,
+                  group2: 2,
+                  group3: 2,
+                  group4: 2,
+                  group5: 2,
+                }}
+                columns={{
+                  group0: 8,
+                  group1: 8,
+                  group2: 8,
+                  group3: 8,
+                  group4: 8,
+                  group5: 8,
+                }}
+              >
+                <BulletedList>{list}</BulletedList>
+              </Grid>
+            );
 
           default:
             break;
         }
       })}
-    </div>
+    </Grid>
   ) : (
     <div>loading ...</div>
   );
