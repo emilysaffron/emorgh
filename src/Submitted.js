df72266eb402c230e7105198597676dca3b141bc
emilysaffron <emiy.saffron@bbc.co.uk>
2020-12-07 11:34:52 +0000

    7 column grid for submitted page

--- src/Submitted.js
+++ src/Submitted.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { SubHeading } from "@bbc/psammead-headings";
+import Grid from "@bbc/psammead-grid";
+import styled from "@emotion/styled";
 import Loader from "react-loader-spinner";
 import tick from "./tick.png";
+
+
+const StyledImg = styled.img`
+  margin-top: 300px;
+`;
+const StyledLoader = styled(Loader)`
+  margin-top: 300px;
+`;
 const Submitted = () => {
   const [loading, updateLoading] = useState(true);
 
@@ -12,12 +22,95 @@ const Submitted = () => {
   }, []);
 
   return loading ? (
-    <Loader type="ThreeDots" color="lightgray" height={80} width={80} />
+    <Grid
+      enableGelGutters
+      columns={{
+        group0: 7,
+        group1: 7,
+        group2: 7,
+        group3: 7,
+        group4: 7,
+        group5: 7,
+      }}
+      margins={{
+        group0: true,
+        group1: true,
+        group2: true,
+        group3: true,
+        group4: true,
+        group5: true,
+      }}
+    >
+      <Grid
+        item
+        startOffset={{
+          group0: 4,
+          group1: 4,
+          group2: 4,
+          group3: 4,
+          group4: 4,
+          group5: 4,
+        }}
+        columns={{
+          group0: 8,
+          group1: 8,
+          group2: 8,
+          group3: 8,
+          group4: 8,
+          group5: 8,
+        }}
+      >
+        <StyledLoader
+          type="ThreeDots"
+          color="lightgray"
+          height={80}
+          width={80}
+        />
+      </Grid>
+    </Grid>
   ) : (
-    <div>
-      <img src={tick} height="100px" width="100px" alt="tick" />
-      <SubHeading>Submitted</SubHeading>
-    </div>
+    <Grid
+      enableGelGutters
+      columns={{
+        group0: 7,
+        group1: 7,
+        group2: 7,
+        group3: 7,
+        group4: 7,
+        group5: 7,
+      }}
+      margins={{
+        group0: true,
+        group1: true,
+        group2: true,
+        group3: true,
+        group4: true,
+        group5: true,
+      }}
+    >
+      <Grid
+        item
+        startOffset={{
+          group0: 4,
+          group1: 4,
+          group2: 4,
+          group3: 4,
+          group4: 4,
+          group5: 4,
+        }}
+        columns={{
+          group0: 8,
+          group1: 8,
+          group2: 8,
+          group3: 8,
+          group4: 8,
+          group5: 8,
+        }}
+      >
+        <StyledImg src={tick} height="100px" width="100px" alt="tick" />
+        <SubHeading>Submitted</SubHeading>
+      </Grid>
+    </Grid>
   );
 };
 
