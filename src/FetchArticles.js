17458781ceef3dc0f123e19d5db05faaf36ae1e1
emilysaffron <emiy.saffron@bbc.co.uk>
2020-12-15 16:54:08 +0000

    dynamically pass pagenum for data

--- src/FetchArticles.js
+++ src/FetchArticles.js
@@ -3,36 +3,10 @@ const getData = (num) => {
 
   let article = "";
   // This can be made much simpler - how can you pass num dynamically into the string?
-  switch (num) {
-    case 1:
-      data = require("./data/article-1.json");
-      article = data.body;
-      return article;
 
-    case 2:
-      data = require("./data/article-2.json");
-      article = data.body;
-      return article;
-
-    case 3:
-      data = require("./data/article-3.json");
-      article = data.body;
-      return article;
-
-    case 4:
-      data = require("./data/article-4.json");
-      article = data.body;
-      return article;
-
-    case 5:
-      data = require("./data/article-5.json");
-      article = data.body;
-      return article;
-
-    default:
-      console.log("here1");
-      break;
-  }
+  data = require(`./data/article-${num}.json`);
+  article = data.body;
+  return article;
 };
 
 export default getData;
