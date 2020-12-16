12facfb6db2afebc5a9d3138cd19c2d77449e4a5
emilysaffron <emiy.saffron@bbc.co.uk>
2020-12-16 13:41:24 +0000

    loading component

---
+++ src/Components/Loading.js
@@ -0,0 +1,32 @@
+import getGridGroups from "../Helpers/GetGridGroups";
+import Grid from "@bbc/psammead-grid";
+import styled from "@emotion/styled";
+import Loader from "react-loader-spinner";
+const StyledLoader = styled(Loader)`
+  margin-top: 300px;
+`;
+
+const Loading = () => {
+  return (
+    <Grid
+      enableGelGutters
+      columns={getGridGroups([7, 7, 7, 7, 7, 7])}
+      margins={getGridGroups([true, true, true, true, true, true])}
+    >
+      <Grid
+        item
+        startOffset={getGridGroups([4, 4, 4, 4, 4, 4])}
+        columns={getGridGroups([8, 8, 8, 8, 8, 8])}
+      >
+        <StyledLoader
+          type="ThreeDots"
+          color="lightgray"
+          height={80}
+          width={80}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Loading;
--- src/Pages/SubmittedPage.js
+++ src/Pages/SubmittedPage.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from "react";
 import { SubHeading } from "@bbc/psammead-headings";
 import Grid from "@bbc/psammead-grid";
 import getGridGroups from "../Helpers/GetGridGroups";
+import Loading from "../Components/Loading";
 import styled from "@emotion/styled";
-import Loader from "react-loader-spinner";
 import tick from "./tick.png";
 
 const StyledImg = styled.img`
-  margin-top: 300px;
-`;
-const StyledLoader = styled(Loader)`
   margin-top: 300px;
 `;
 
@@ -23,25 +20,7 @@ const SubmittedPage = () => {
   }, []);
 
   return loading ? (
-    // This could be its own <Loading> component
-    <Grid
-      enableGelGutters
-      columns={getGridGroups([7, 7, 7, 7, 7, 7])}
-      margins={getGridGroups([true, true, true, true, true, true])}
-    >
-      <Grid
-        item
-        startOffset={getGridGroups([4, 4, 4, 4, 4, 4])}
-        columns={getGridGroups([8, 8, 8, 8, 8, 8])}
-      >
-        <StyledLoader
-          type="ThreeDots"
-          color="lightgray"
-          height={80}
-          width={80}
-        />
-      </Grid>
-    </Grid>
+    <Loading />
   ) : (
     <Grid
       enableGelGutters
