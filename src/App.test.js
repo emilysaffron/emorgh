d479d6f7205e1fd958ce8722b033fdd9b5c01bdc
emilysaffron <emiy.saffron@bbc.co.uk>
2020-12-18 12:43:39 +0000

    CalculatePageNumber Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,20 @@
+import add from "./Function";
 import CalculatePageNumber from "./Helpers/CalculatePageNumber";
 test("faketest", () => {
   expect(true).toBeTruthy();
 });
+
+test("add", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+//Unit Tests
+
 test("CalculatePageNumber", () => {
   expect(CalculatePageNumber(3, "Next")).toBe(4);
   expect(CalculatePageNumber(3, "Previous")).toBe(2);
   expect(CalculatePageNumber(5, "Next")).toBe(5);
   expect(CalculatePageNumber(1, "Previous")).toBe(1);
 });
+
+
